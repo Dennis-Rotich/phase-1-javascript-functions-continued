f4f37467139f7c9bd20c08d3d2f91d5e2e796bd0
Dennis <denkipyator@gmail.com>
2023-12-13 13:06:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function saturdayFun(saturdayactivity = 'roller-skate'){
+    return("This Saturday, I want to " + saturdayactivity + "!"); 
+}
+function mondayWork(mondayActivity = 'go to the office'){
+   return('This Monday, I will ' + mondayActivity + '.');
+}
+function wrapAdjective(character = "*"){
+    return function(theAdjective  = "Special"){
+        return("You are " + character + theAdjective + character + '!');
+    };
+}
+
 // code your solution here
